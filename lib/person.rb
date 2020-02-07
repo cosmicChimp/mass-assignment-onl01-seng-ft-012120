c8bcfea4e9d30e8a2e4de2fdfe2cbda3ca1f98bb
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-07 06:32:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,28 @@
 class Person
-  attr_accessor
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
+    self.each do |k, v|
+      
     
   end
   
+  
+  avi_attributes {
+      :name => "Avi",
+      :birthday => "01/29/1984",
+      :hair_color => "brown",
+      :eye_color => "brown",
+      :height => "short",
+      :weight => "good",
+      :handed => "lefty",
+      :complexion => "decent",
+      :t_shirt_size => "medium",
+      :wrist_size => "small",
+      :glove_size => "normal",
+      :pant_length => "32",
+      :pant_width => "32"
+    }
   
 end
 
