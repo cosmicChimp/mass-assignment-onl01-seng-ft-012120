4481a79e7389488aa1c9d5724390de44511829b4
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-07 06:41:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,23 +5,7 @@ class Person
     attributes.each {|k, v| self.send ("#{k}=", v)}
   end
   
-  
-  # avi_attributes {
-  #     :name => "Avi"
-  #     :birthday => "01/29/1984"
-  #     :hair_color => "brown"
-  #     :eye_color => "brown"
-  #     :height => "short"
-  #     :weight => "good"
-  #     :handed => "lefty"
-  #     :complexion => "decent"
-  #     :t_shirt_size => "medium"
-  #     :wrist_size => "small"
-  #     :glove_size => "normal"
-  #     :pant_length => "32"
-  #     :pant_width => "32"
-  #   }
-  # end
+
   
 end
 
