f498e6a978c45008cd21b39f1e1c509bfc3f6b63
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-07 06:36:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,18 +7,18 @@ class Person
   
   
   avi_attributes {
-      :name => "Avi",
-      :birthday => "01/29/1984",
-      :hair_color => "brown",
-      :eye_color => "brown",
-      :height => "short",
-      :weight => "good",
-      :handed => "lefty",
-      :complexion => "decent",
-      :t_shirt_size => "medium",
-      :wrist_size => "small",
-      :glove_size => "normal",
-      :pant_length => "32",
+      :name => "Avi"
+      :birthday => "01/29/1984"
+      :hair_color => "brown"
+      :eye_color => "brown"
+      :height => "short"
+      :weight => "good"
+      :handed => "lefty"
+      :complexion => "decent"
+      :t_shirt_size => "medium"
+      :wrist_size => "small"
+      :glove_size => "normal"
+      :pant_length => "32"
       :pant_width => "32"
     }
   end
